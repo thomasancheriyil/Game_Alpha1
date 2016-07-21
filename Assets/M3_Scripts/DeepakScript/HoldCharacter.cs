107424a0987f8b49d5d430acfdc413170c1a0590
daCoolestNerd <bsawyer@gatech.edu>
2016-07-21 00:19:19 -0400

    Tutorial level complete
    Deepak tutorial level imported with kaya prefab and camera prefab added.
    play testing worked.
    
    standard scaling for kaya is 0.7,0.7,0.7 with a jump power of 7.5 (bit
    high for realism but okay..)
    
    controller bug is that the player collider will get stuck on
    walls...need to update after alpha

---
+++ Assets/M3_Scripts/DeepakScript/HoldCharacter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldCharacter : MonoBehaviour {
+    
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.parent = gameObject.transform;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        col.transform.parent = null;
+    }
+
+}
